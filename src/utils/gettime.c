451b2e6c777f6acb1dccfc8f5f2f3e226cfbde52
Eliam <Texsword76@gmail.com>
2025-01-27 15:44:50 +0100

    ajout : configuration du workflow GitHub pour la vérification de la norme et la compilation du projet

---
+++ src/utils/gettime.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gettime.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edetoh <edetoh@student.42lehavre.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/13 12:14:34 by edetoh            #+#    #+#             */
+/*   Updated: 2025/01/27 15:42:33 by edetoh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../philosophers.h"
+
+unsigned int	get_current_time(void)
+{
+	struct timeval	time;
+
+	if (gettimeofday(&time, NULL) == -1)
+		write(2, "gettimeofday() error\n", 22);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
+}
+
+int	ft_usleep(size_t milliseconds)
+{
+	size_t	start;
+
+	start = get_current_time();
+	while ((get_current_time() - start) < milliseconds)
+		usleep(100);
+	return (0);
+}
